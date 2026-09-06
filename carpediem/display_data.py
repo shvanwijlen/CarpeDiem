d792d36da03e75812a15179350b462b3549e29cd
Simon van Wijlen <simon@vanwijlen.com>
2026-09-06 14:53:58 +0200

    Adding two MQTT system/0 values for redundancy (we use the modbus primarily)

--- carpediem/display_data.py
+++ carpediem/display_data.py
@@ -45,6 +45,8 @@ _FIELD_DEFINITIONS = [
     ("Battery1 Current (A)", "Battery1 Current (A)"),
     ("Battery Time to Go (System)", "TTG (System)"),
     ("Battery Time to Go (Batt)", "TTG (Batt)"),
+    ("Battery system SOC (%)", "Battery system SOC (%)"), #redundant as we use the SOC data for display from another source, but we keep it here for completeness
+    ("Battery system Voltage (V)", "Battery system Voltage (V)"), #redundant as we use the voltage data for display from another source, but we keep it here for completeness
     ("DC Power (W)", "DC Power (W)"),
     ("DC Current (A)", "DC Current (A)"),
     ("PV Power (W)", "PV Power (W)"),
