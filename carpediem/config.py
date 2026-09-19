ab44317c528ebc7a904a407d97290398d80dcc36
Simon van Wijlen <simon@vanwijlen.com>
2026-09-19 14:09:59 +0200

    l

--- carpediem/config.py
+++ carpediem/config.py
@@ -62,6 +62,10 @@ class FeatureFlags:
     do_vaarweg: bool = field(default_factory=lambda: _bool("CARPEDIEM_DO_VAARWEG", True))
     do_gpx_log: bool = field(default_factory=lambda: _bool("CARPEDIEM_DO_GPX_LOG", True))
     do_show: bool = field(default_factory=lambda: _bool("CARPEDIEM_DO_SHOW", True))
+    if do_show:
+       log(1, "CARPEDIEM_DO_SHOW is True - the display will be on even if other flags are off and data values are logged")
+    else:
+         log(1, "CARPEDIEM_DO_SHOW is False - the display will be off even if other flags are on and data values are not logged")
 
     use_rtc: bool = field(default_factory=lambda: _bool("CARPEDIEM_USE_RTC", False))
     use_matrix: bool = field(default_factory=lambda: _bool("CARPEDIEM_USE_MATRIX", False))
